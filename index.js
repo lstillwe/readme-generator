b2c10415e54dc1b4e16f9602fd9fbdc7c8d19931
Lisa Stillwell <lisastillwell@bellsouth.net>
2021-12-10 17:54:01 -0500

    #1 Add user questions

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const gm = require('./utils/generateMarkdown');
+
+const licenses = ['None', 'MIT License', 'BSD 3 License', 'GPL 3.0 License']
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter the installation instructions:',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the information on how to use this project:'
+    },
+    {
+        type: 'input',
+        name: 'guidelines',
+        message: 'Enter the contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter the test instructions:',
+        default: 'npm test'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Enter project license:',
+        choices: licenses
+    },
+    {
+        type: 'input',
+        message: 'Enter your github username:',
+        name: 'username'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'email',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
